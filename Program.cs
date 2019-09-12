806d8676e51dd79887d7eb14a4ac0500198b4e78
D-RaMedia <54799111+D-RaMedia@users.noreply.github.com>
2019-09-12 12:01:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Clear();
+
+            Console.WriteLine("================================================================");
+            Console.WriteLine("========================             ===========================");
+            Console.WriteLine("========================   Welkom    ===========================");
+            Console.WriteLine("========================             ===========================");
+            Console.WriteLine("================================================================");
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
